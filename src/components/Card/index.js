3fe47fdd5c76cd53568724ebf36d271b223d561f
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-06-12 15:56:17 -0300

    Improved the code responsible to give the rank image uri

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { SvgUri } from 'react-native-svg';
+
 import api from '~/services/api';
 
 import { Container, ImageArea, RankImg, Avatar } from './styles';
 
 export default function Card({ dataR6 }) {
-  const [playerData, setPlayerData] = useState({});
+  const [playerData, setPlayerData] = useState([]);
 
   useEffect(() => {
     async function getPlayerData() {
@@ -20,50 +21,54 @@ export default function Card({ dataR6 }) {
           },
         });
 
-        const { data } = response;
+        if (response) {
+          const { data } = response;
 
-        const { ncsa, emea, apac } = data.seasons[
-          Object.keys(data.seasons)[0]
-        ].regions;
+          const { ncsa, emea, apac } = data.seasons[
+            Object.keys(data.seasons)[0]
+          ].regions;
 
-        switch (dataR6.region) {
-          case 'South America':
-          case 'North America':
-            setPlayerData({
-              username: data.username,
-              uri: data.avatar_url_256,
-              seasonData: ncsa[0], // It will return the most updated data from this user.
-            });
-            break;
-          case 'Europe':
-          case 'Africa':
-            setPlayerData({
-              username: data.username,
-              uri: data.avatar_url_256,
-              seasonData: emea[0], // It will return the most updated data from this user.
-            });
-            break;
-          case 'Asia':
-          case 'Oceania':
-            setPlayerData({
-              username: data.username,
-              uri: data.avatar_url_256,
-              seasonData: apac[0], // It will return the most updated data from this user.
-            });
-            break;
-          default:
-            setPlayerData({
-              uri: data.avatar_url_256,
-              ncsa,
-              emea,
-              apac,
-            });
+          switch (dataR6.region) {
+            case 'South America':
+            case 'North America':
+              setPlayerData({
+                username: data.username,
+                uri: data.avatar_url_256,
+                rank_image: ncsa[0].rank_image, // It will return the most updated data from this user.
+              });
+              break;
+            case 'Europe':
+            case 'Africa':
+              setPlayerData({
+                username: data.username,
+                uri: data.avatar_url_256,
+                rank_image: emea[0].rank_image, // It will return the most updated data from this user.
+              });
+              break;
+            case 'Asia':
+            case 'Oceania':
+              setPlayerData({
+                username: data.username,
+                uri: data.avatar_url_256,
+                rank_image: apac[0].rank_image, // It will return the most updated data from this user.
+              });
+              break;
+            default:
+              setPlayerData({
+                uri: data.avatar_url_256,
+                ncsa,
+                emea,
+                apac,
+              });
+          }
         }
       }
     }
 
     getPlayerData();
   }, [dataR6]);
+
+  console.tron.log(playerData);
 
   return (
     <Container>
@@ -75,8 +80,13 @@ export default function Card({ dataR6 }) {
               : 'https://api.adorable.io/avatars/50/abott@adorable.png',
           }}
         />
-        {/* <SvgUri uri="https://cdn.r6stats.com/seasons/ranks/champions.svgg" /> */}
       </ImageArea>
+      <SvgUri
+        width="100px"
+        height="100px"
+        uri="https://cdn.r6stats.com/seasons/ranks/champions.svg"
+      />
+      {/* <SvgUri uri="https://cdn.r6stats.com/seasons/ranks/champions.svg" /> */}
     </Container>
   );
 }
